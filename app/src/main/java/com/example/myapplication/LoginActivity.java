c9f420ef8094ecc544814842d0ddc6e0c7da88f5
0118200 <donennone@gmail.com>
2025-12-03 18:40:23 +0700

    Update LoginActivity.java

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -15,7 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+//
 public class LoginActivity extends AppCompatActivity {
 
     private EditText inputEmail;
